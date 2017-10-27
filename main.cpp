35854a86417d732f837cedb445048c6aec35311b
Monica Rad <rad_monica@yahoo.com>
2017-10-27 19:45:41 +0300

    Exercitiul 4

--- main.cpp
+++ main.cpp
@@ -9,9 +9,11 @@ int minim(int [], int);
 
 
 int main(){
-    int a[] = {12, -14, 7, 23, 5, -71, 4};
-    schimba(a, 7);   //  In sir sunt 7 elemente
-    int mini = minim(a, 7);
-    cout << "Minimul valorilor dupa schimbarea semnelor este: " << mini << endl;
+    double a[] = {12., -14.2, 7., 23., 5., -71.7, 4.};
+    int rez = caut(a, 7, 23);   //  In sir sunt 7 elemente
+    if (rez<0)
+    cout << "Valoarea lipseste! " << endl;
+    else
+    cout << "Valoarea cautata este pe pozitia: " << rez + 1 << endl;
     return 0;
 }
